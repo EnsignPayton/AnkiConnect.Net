45e493c165976f80b463ae24f91703086237aeae
Logan Payton <Logan.Payton@javs.com>
2022-07-15 22:40:36 -0400

    Revert record, it ugly

--- src/AnkiConnect.Net/Internal/AnkiRequest.cs
+++ src/AnkiConnect.Net/Internal/AnkiRequest.cs
@@ -1,14 +1,30 @@
 ﻿namespace AnkiConnect.Net.Internal;
 
-internal record AnkiRequest(
-    [property: JsonPropertyOrder(0)] [property: JsonPropertyName("action")] string Action,
-    [property: JsonPropertyOrder(1)] [property: JsonPropertyName("version")] int Version = 6);
+internal class AnkiRequest
+{
+    public AnkiRequest(string action)
+    {
+        Action = action;
+    }
 
-internal record AnkiRequest<TParams>(
-        string Action,
-        [property: JsonPropertyOrder(2)]
-        [property: JsonPropertyName("params")]
-        [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        TParams? Params,
-        int Version = 6)
-    : AnkiRequest(Action, Version) where TParams : class;
+    [JsonPropertyOrder(0)]
+    [JsonPropertyName("action")]
+    public string Action { get; set; }
+
+    [JsonPropertyOrder(1)]
+    [JsonPropertyName("version")]
+    public int Version { get; set; } = 6;
+}
+
+internal class AnkiRequest<TParams> : AnkiRequest where TParams : class
+{
+    public AnkiRequest(string action, TParams @params) : base(action)
+    {
+        Params = @params;
+    }
+
+    [JsonPropertyOrder(2)]
+    [JsonPropertyName("params")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public TParams? Params { get; set; }
+}
